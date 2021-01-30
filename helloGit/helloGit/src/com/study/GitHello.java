78b1bd7e56f272869fd9e200cd86d10b58d47cce
huang <1509031142@qq.com>
2021-01-30 15:16:23 +0800

    ss

--- helloGit/helloGit/src/com/study/GitHello.java
+++
@@ -1,8 +0,0 @@
-package com.study;
-
-public class GitHello {
-    public static void main(String[] args) {
-        System.out.println("helloGit");
-        System.out.println("helloWorld");
-    }
-}
